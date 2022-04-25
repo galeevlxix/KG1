a710bdb3e23160d9c4a16b98783c0ba8589df0c2
Timur Galeev <gtimur477@gmail.com>
2022-04-25 10:22:12 +0500

    7

--- KG1/Source.cpp
+++ KG1/Source.cpp
@@ -2,29 +2,66 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include "glm/vec3.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
 
 GLuint VBO;
+GLuint gWorldLocation;
 
-using namespace glm;
+static const char* pVS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+layout (location = 0) in vec3 Position;                                             \n\
+                                                                                    \n\
+uniform mat4 gWorld;                                                                \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
+}";
+
+static const char* pFS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+out vec4 FragColor;                                                                 \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    FragColor = vec4(1.0, 0.0, 1.0, 1.0);                                           \n\
+}";
 
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1.0f, 0.0f, 1.0f);
+
+    static float Scale = 0.0f;
+    Scale += 0.001f;
+
+    glm::mat4  World;
+
+    World[0][0] = cosf(Scale); World[0][1] = -sinf(Scale); World[0][2] = 0.0f; World[0][3] = sinf(0.0f);
+    World[1][0] = sinf(Scale); World[1][1] = cosf(Scale); World[1][2] = 0.0f; World[1][3] = 0.0f;
+    World[2][0] = 0.0f; World[2][1] = 0.0f; World[2][2] = 1.0f; World[2][3] = 0.0f;
+    World[3][0] = 0.0f; World[3][1] = 0.0f; World[3][2] = 0.0f; World[3][3] = 1.0f;
+
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]); 
+
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
     glDrawArrays(GL_POLYGON, 0, 8);
+
     glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
 }
 
-static void createWindow() {
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(1000, 1000);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("eeewdewewd");
+static void InitializeGlutCallbacks()
+{
     glutDisplayFunc(RenderSceneCB);
+    glutIdleFunc(RenderSceneCB);
 }
 
 static void CreateVertexBuffer()
@@ -33,10 +70,10 @@ static void CreateVertexBuffer()
     double t = 1.0;
     const double PI = 3.141592653589793;
 
-    vec3 Vertices[8];
+    glm::vec3 Vertices[8];
 
     for (int i = 0; i < 8; i++) {
-        Vertices[i] = vec3(r * cos(t + i * PI * 0.25), r * sin(t + i * PI * 0.25), 0.0f);
+        Vertices[i] = glm::vec3(r * cos(t + i * PI * 0.25), r * sin(t + i * PI * 0.25), 0.0f);
     }
     
     glGenBuffers(1, &VBO);
@@ -44,13 +81,82 @@ static void CreateVertexBuffer()
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+    GLint Lengths[1];
+    Lengths[0] = strlen(pShaderText);
+    glShaderSource(ShaderObj, 1, p, Lengths);
+    glCompileShader(ShaderObj);
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
+    AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+
+    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+    assert(gWorldLocation != 0xFFFFFFFF);
+}
+
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-    createWindow();
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(832, 624);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("rewrewregf");
 
+    InitializeGlutCallbacks();
+
+    // Must be done after glut is initialized!
     GLenum res = glewInit();
-
     if (res != GLEW_OK) {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
@@ -59,6 +165,8 @@ int main(int argc, char** argv)
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
     CreateVertexBuffer();
+
+    CompileShaders();
 
     glutMainLoop();
 
