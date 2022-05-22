fde1c7afd7e3f5ae789a838eafc4fdebba6f2672
Galeev LXIX <gtimur477@gmail.com>
2022-05-23 03:49:34 +0500

    исправлена картинка

--- KG1/pipeline.cpp
+++ KG1/pipeline.cpp
@@ -7,11 +7,8 @@ const Matrix4f& Pipeline::getTransformation()
     scaleTrans.InitScaleTransform(mScale.x, mScale.y, mScale.z);
     rotateTrans.InitRotateTransform(mRotateInfo.x, mRotateInfo.y, mRotateInfo.z);
     translationTrans.InitTranslationTransform(mWorldPos.x, mWorldPos.y, mWorldPos.z);
-    CameraTranslationTrans.InitTranslationTransform(-m_camera.Pos.x, -m_camera.Pos.y, -m_camera.Pos.z);
-    CameraRotateTrans.InitCameraTransform(m_camera.Target, m_camera.Up);
-    persProjTrans.InitPersProjTransform(mPersProj.FOV, mPersProj.width, mPersProj.height, mPersProj.zNear, mPersProj.zFar);
     
-    mTransformation = persProjTrans * CameraRotateTrans * CameraTranslationTrans * translationTrans * rotateTrans * scaleTrans;
+    mTransformation = translationTrans * rotateTrans * scaleTrans;
     return mTransformation;
 }
 
